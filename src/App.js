be3ebbd3fadf7e342023e9fe1aa4b6ddb7cb6447
megbug <meghub@proton.me>
2023-04-06 17:49:36 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [count, setCount] = useState(0);
+	const [number, setNumber] = useState();
+
+	const handleNumber = (event) => {
+		setNumber(Number(event.target.value))
+	}
+
+	const add = () => {
+		setCount(count + number);
+	}
+
+	const sub = () => {
+		setCount(count - number);
+	}
+
+	return (
+		<div className="App">
+			<body>
+				<h1 id='title'>Banana Bank</h1>
+				<div className='konto'>
+					<h2>Dein Girokonto</h2>
+					<p className='saldo'>{count}</p>
+					<input className='geldbetrag' type="number" name="" id="" onChange={handleNumber} placeholder='Höhe Betrag' />
+					<button className='einzahlen' onClick={add}>Einzahlen</button>
+					<button className='auszahlen' onClick={sub}>Auszahlen</button>
+				</div>
+			</body>
+		</div>
+	);
 }
 
 export default App;
+
+
+
+
